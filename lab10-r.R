73b2ce4d93b68c9ebf44c501db8f00f9454579d9
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2025-02-14 11:54:21 +0530

    Add files via upload

---
+++ lab10-r.R
@@ -0,0 +1,7 @@
+# 10. Find the maximum and minimum value of a given vector
+vector <- sample(1:100, 10)
+max_value <- max(vector)
+min_value <- min(vector)
+cat("Vector:", vector, "\n")
+cat("Maximum value:", max_value, "\n")
+cat("Minimum value:", min_value, "\n")
