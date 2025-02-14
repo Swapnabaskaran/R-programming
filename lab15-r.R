5c1cdaae01224f7247285d3d317403ec32195849
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2025-02-14 12:37:33 +0530

    Add files via upload

---
+++ lab15-r.R
@@ -0,0 +1,13 @@
+#15. Create Three Vectors (Numeric, Character, Logical) and Display Types
+num_vec <- c(1.1, 2.2, 3.3)
+char_vec <- c("A", "B", "C")
+log_vec <- c(TRUE, FALSE, TRUE)
+
+print(num_vec)
+print(class(num_vec))
+
+print(char_vec)
+print(class(char_vec))
+
+print(log_vec)
+print(class(log_vec))
