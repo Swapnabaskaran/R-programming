b8d41b5ae2894451555a644d857cec8ea63cf59e
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2025-02-14 11:50:27 +0530

    Add files via upload

---
+++ lab5-r.R
@@ -0,0 +1,9 @@
+# 5. Get the first 10 Fibonacci numbers
+fibonacci <- numeric(10)
+fibonacci[1] <- 0
+fibonacci[2] <- 1
+for (i in 3:10) {
+  fibonacci[i] <- fibonacci[i-1] + fibonacci[i-2]
+}
+cat("First 10 Fibonacci numbers:", fibonacci, "\n")
+
