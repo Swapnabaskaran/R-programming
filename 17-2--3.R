24f83f265244ede7624cf1708eefc3fc4d17de86
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2025-02-17 15:03:40 +0530

    Add files via upload

---
+++ 17-2--3.R
@@ -0,0 +1,10 @@
+#17/3(3)
+# Define four columns of data
+data <- c(1:24)
+
+# Create an array with 3 rows, 4 columns, and 2 tables
+my_array <- array(data, dim = c(3, 4, 2))
+
+# Display the content of the array
+print("Created 3D Array:")
+print(my_array)
