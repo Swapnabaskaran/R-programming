2878dceaf9bee01667ed2a06581dbca6a5551ee0
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2025-02-17 14:58:36 +0530

    Add files via upload

---
+++ lab17-2--1.R
@@ -0,0 +1,19 @@
+#17/2(1)
+#Define two vectors
+vector1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+vector2 <- c(10, 11, 12, 13, 14, 15, 16, 17, 18)
+
+# Create a 3x3x2 array
+my_array <- array(c(vector1, vector2), dim = c(3, 3, 2))
+
+# Print the array
+print("Created Array:")
+print(my_array)
+
+# Print the second row of the second matrix
+print("Second row of the second matrix:")
+print(my_array[2, , 2])
+
+# Print the element in the 3rd row and 3rd column of the 1st matrix
+print("Element in the 3rd row and 3rd column of the 1st matrix:")
+print(my_array[3, 3, 1])
