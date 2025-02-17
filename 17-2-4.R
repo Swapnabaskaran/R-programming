615dc0ce17dd67a392ea06c00775a0517535d061
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2025-02-17 15:04:07 +0530

    Add files via upload

---
+++ 17-2-4.R
@@ -0,0 +1,10 @@
+#17/3(4)
+# Generate even numbers greater than 50
+even_numbers <- seq(52, 82, by = 2)
+
+# Create a 5x3 array
+array_even <- matrix(even_numbers, nrow = 5, ncol = 3)
+
+# Print the array
+print("5x3 Array of Even Numbers Greater than 50:")
+print(array_even)
